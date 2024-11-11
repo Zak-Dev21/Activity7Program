94291b6526b80dca72dc8dade3ee17e8117652e4
Zak <zakariabouaddou@gmail.com>
2024-11-11 13:09:05 -0800

    modified code to  utilize getline() and also have detailed comments explaining code line by line.

--- Activity7Program.cpp
+++ Activity7Program.cpp
@@ -1,32 +1,32 @@
-
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	// store variables that we later be used for inputs
-	string first_name;
-	string second_name;
-	string third_name;
-	string names[4];
+    // store variables that we later be used for inputs
+    string first_name;
+    string second_name;
+    string third_name;
+    string names[4];
 
-	cout << "Give me one name of your choice: " << endl; // ask user to input data
-	cin >> first_name;
+    // Using getline to take full names as input
+    cout << "Give me one name of your choice: " << endl;
+    getline(cin, first_name);  // Read a full line
 
-	cout << "Give me annother name of your choice: " << endl; // ask user to input data
-	cin >> second_name;
+    cout << "Give me another name of your choice: " << endl;
+    getline(cin, second_name); // Read a full line
 
-	cout << "Give me one last name of your choice: " << endl; // ask user to input data
-	cin >> third_name;
+    cout << "Give me one last name of your choice: " << endl;
+    getline(cin, third_name);  // Read a full line
 
-	// storing inputs given in the names of size 4 array
-	names[0] = first_name;
-	names[1] = second_name;
-	names[2] = third_name;
+    // storing inputs in the names array
+    names[0] = first_name;
+    names[1] = second_name;
+    names[2] = third_name;
 
-	cout << endl << "First name you gave is: " << names[0];
+    cout << endl << "First name you gave is: " << names[0] << endl;
 
-
-
-
+    return 0;
 }
+
